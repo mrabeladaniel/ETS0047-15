d5dc5ad588e03b718c16d823de7d5dcc5d78d85e
mrabeladaniel <abeladaniel24@gmail.com>
2025-03-31 21:24:01 -0700

    String Methods In Python

--- String Methods In Python 5.py
+++ String Methods In Python 5.py
@@ -1,38 +1,28 @@
-#13. str.lstrip()
-#This method removes leading whitespace (spaces, tabs, newlines) from the string. 
-#And also helps to pass a string argument to specify the characters to be removed from the start of the string.
+#16.str.join():
+#This method is used to join elements of an iterable (like a list or tuple) into a single string. 
+#The string that calls the method is used as a separator.
 #Example:
-text = "   Hello World!"
-print(text.lstrip()) 
-# Output:
-#"Hello World!"
-
-# Removing specific characters:
-text = "xxxyyyHello World!"
-print(text.lstrip("xy"))  
- #Output:
-#"Hello World!"
-
-#14.str.rstrip()
-#This method removes trailing whitespace (spaces, tabs, newlines) from the string. 
-#Like lstrip(), this method also helps to pass a string argument to specify the characters to be removed from the end of the string.
-
+words = ["Hello", "World"]
+result = " ".join(words)
+print(result)  
+# Output: "Hello World"
+#17. str.isalpha():
+#This method checks if all the characters in the string are alphabetic (i.e., they are letters).
+#It returns True if all characters are alphabetic and there is at least one character, otherwise it returns False.
 #Example:
-
-text = "Hello World!   "
-print(text.rstrip()) 
-# Output: "Hello World!"
-
-#Removing specific characters:
-text = "Hello World!xxx"
-print(text.rstrip("x")) 
- #Output: "Hello World!"
-#15.str.split()
-#this method splits a string into a list of substrings based on a specified delimiter (default is whitespace).
-#EXample:
-text = "apple orange banana"
-result = text.split()
-print(result)
-
-#Output:
-#['apple', 'orange', 'banana']
+text = "Hello"
+print(text.isalpha())
+  # Output: True
+text = "Hello123"
+print(text.isalpha())
+  # Output: False
+#18. str.isdigit():
+#This method checks if all the characters in the string are digits.
+#It returns True if all characters are digits and there is at least one character, otherwise it returns False.
+#Example:
+text = "12345"
+print(text.isdigit()) 
+ # Output: True
+text = "123a5"
+print(text.isdigit()) 
+ # Output: False
