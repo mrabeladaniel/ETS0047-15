464ca0c75ec681cba7bedc5cf1478212e1cb82d6
mrabeladaniel <abeladaniel24@gmail.com>
2025-03-31 20:49:53 -0700

    string methods in python 7

---
+++ String methods.py
@@ -0,0 +1,35 @@
+#1. upper()
+#The upper() method converts all characters in a string to uppercase.
+
+#Example:
+
+text = "hello world"
+result = text.upper()
+print(result)
+#Output:
+
+
+# HELLO WORLD
+#2. lower()
+#The lower() method converts all characters in a string to lowercase.
+
+#Example:
+
+
+text = "HELLO WORLD"
+result = text.lower()
+print(result)
+#Output:
+#hello world
+
+#3. replace()
+#The replace() method replaces a specified substring with another substring in the string.
+
+#Example:
+
+text = "I like cats"
+result = text.replace("cats", "dogs")
+print(result)
+
+#Output:
+#I like dogs
