7175e87dfc8800b898c28b6dd5d2df66741cd979
mrabeladaniel <abeladaniel24@gmail.com>
2025-04-10 01:12:49 -0700

    list methods in python with examples

---
+++ List Methods In Python 2.py
@@ -0,0 +1,25 @@
+#1. count()
+#Returns the number of times a specified value appears in the list.
+#Examples:
+fruits = ['apple', 'banana', 'apple', 'orange']
+print(fruits.count('apple')) 
+
+ # Output: 2
+
+#2. extend()
+#Adds the elements of an iterable (e.g., list, tuple, string) to the end of the list.
+#Examples:
+numbers = [1, 2, 3]
+more_numbers = [4, 5, 6]
+numbers.extend(more_numbers)
+print(numbers)  
+
+# Output: [1, 2, 3, 4, 5, 6]
+
+#3. Definition:
+#Returns the index of the first element with the specified value.
+#Examples"
+colors = ['red', 'blue', 'green', 'blue']
+print(colors.index('blue')) 
+
+ # Output: 1 
