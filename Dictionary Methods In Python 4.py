b6f947e3bc48aaf1879efd67f8d67255e4a9b3ac
mrabeladaniel <abeladaniel24@gmail.com>
2025-04-21 20:43:10 -0700

    defined and gave examples in dictionary methods in python

---
+++ Dictionary Methods In Python 4.py
@@ -0,0 +1,14 @@
+#1. update()
+ #Adds or updates key-value pairs from another dictionary.
+#Example:
+d = {'a': 1, 'b': 2}
+d.update({'b': 3, 'c': 4})
+print(d) 
+ # Output: {'a': 1, 'b': 3, 'c': 4}
+#2. values()
+#Returns all the values in the dictionary.
+#Example:
+d = {'x': 10, 'y': 20}
+print(d.values()) 
+ # Output: dict_values([10, 20])
+
