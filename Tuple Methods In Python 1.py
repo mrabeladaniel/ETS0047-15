22e61564f66e6991e1ce1d3473214df3899cf62c
mrabeladaniel <abeladaniel24@gmail.com>
2025-04-22 21:48:42 -0700

    defined and gave examples on tuple methods in python

---
+++ Tuple Methods In Python 1.py
@@ -0,0 +1,13 @@
+#1. count()
+#Returns how many times a value appears in the tuple.
+#Example:
+t = (1, 2, 3, 2, 4, 2)
+print(t.count(2)) 
+ # Output: 3
+#2. index()
+ #Returns the index of the first occurrence of a value.
+#Example:
+t = (5, 3, 7, 3, 9)
+print(t.index(3))
+  # Output: 1
+
